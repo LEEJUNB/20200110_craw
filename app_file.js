26de148a9c53eebed20428c52e78c9c5f96d791b
junblee <topguide82@gmail.com>
2019-12-17 23:06:05 +0900

    write list

--- app_file.js
+++ app_file.js
@@ -1,6 +1,7 @@
 const express_id = require('express'); // require함수가 리턴한 값(express)을 express_id라는 변수에 담았다. 이 변수로 express모듈 제어가능
 const app = express_id(); // express모듈을 가져왔으면 app객체를 만든다. express_id변수에 담긴 함수를 호출, 실행하면 app변수, 객체를 리턴
 const bodyParser = require('body-parser');
+const fs = require('fs');
 
 app.use(bodyParser.urlencoded({extended : false}));
 app.locals.pretty = true;
@@ -12,12 +13,31 @@ app.get('/topic/new', function(req,res){
     res.render('new');
 });
 
+// form에 입력한 값들이 파일로 저장되도록 fs모듈을 이용하자
 app.post('/topic', function(req,res){
     var title = req.body.title;
     var description = req.body.description;
-    res.send(`new POst!! ${title} ${description}`);
-})
+    //data라는 폴더에 형성된다.
+    fs.writeFile('data/'+title, description, function(err){
+        if(err){
+            res.status(500).send('Internal Server Error'); // 500 is error number
+        }
+        res.send('Success!' + req.body.title);
+    })
+});
 
-app.listen(4000, function(){ //app객체가 가진 메소드 중 listen을 통해 특정 port를 listen하도록 만든다.
+// 사용자가 url을 통해 들어올 수 있도록 get도 만들자.
+// 글 목록이 화면에 표시된다.
+app.get('/topic', function(req,res){
+    fs.readdir('data', function(err,files){ // files인자 안에는 data라는 디렉터리안에 포함된 파일들이 배열로 담김
+        if(err){
+            console.log(err);
+            res.status(500).send('Internal Server Error');
+        }
+        res.render('view', {topics:files}); // view is filename, topics(변수)를 통해 files인자(파일들을 배열화시킨 것)를 가져온다.
+    })
+});
+
+app.listen(3000, function(){ //app객체가 가진 메소드 중 listen을 통해 특정 port를 listen하도록 만든다.
     console.log('Connected, 3000 port!');
 })
--- index.js
+++ index.js
@@ -2,13 +2,13 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const bodyParser = require('body-parser');
+const fs = require('fs');
+
+app.use(bodyParser.urlencoded({extended:false}));
 
 app.locals.pretty = true;
 app.set('view engine', 'pug');
 app.set('views','./views'); // folder name is 'views', directory position is './views'
-
-
-app.use(bodyParser.urlencoded({extended:false}));
 
 //그럼 public폴더안에 있는 파일은 render할때 못부르는건가?
 app.use(express.static('public')); // docs를 찾을때 public에서 찾음
@@ -20,9 +20,7 @@ app.get('/template', function(req,res){ // render될 파일은 ./views폴더에 
     res.render('temp', {variableMan:'Var', Time:Date(), _title:'Pug Page'}); // file name is 'temp'
 })
 
-
 app.get('/topic/:id', function(req,res){
-
     var topics = [
         'What it is?',
         'Our Goal',
