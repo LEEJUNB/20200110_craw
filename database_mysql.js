9fb3f242af0d7e3f18485fce20ff1ab54ca3b740
junblee <topguide82@gmail.com>
2019-12-26 13:17:31 +0900

    connect mysql

--- database_mysql.js
+++ database_mysql.js
@@ -1,9 +1,19 @@
-require('mysql');
-var conn =
-mysql.createConnection({
-  host : 'localhost',
-  user : 'root',
+var mysql      = require('mysql');
+var conn = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
   password : '111111',
   database : 'o2'
 });
+
 conn.connect();
+
+const sql = 'SELECT * FROM topic';
+conn.query(sql, function(err, rows, fields){
+  if(err){
+    console.log(err);
+  } else {
+    console.log('row', rows);
+    console.log('fields', fields);
+  }
+})
