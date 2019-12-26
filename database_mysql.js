07aaccd1e5add9f4a4bf1827c23d80d14b5027da
junblee <topguide82@gmail.com>
2019-12-26 16:02:01 +0900

    sql update delete

--- database_mysql.js
+++ database_mysql.js
@@ -8,16 +8,20 @@ var conn = mysql.createConnection({
 
 conn.connect();
 
-// const sql = 'SELECT * FROM topic';
-// conn.query(sql, function(err, rows, fields){
-//   if(err){
-//     console.log(err);
-//   } else {
-//     for(var i = 0; i<rows.length; i++){
-//       console.log(rows[i].author);
-//     }
-//   }
-// })
+/*
+const sql = 'SELECT * FROM topic';
+conn.query(sql, function(err, rows, fields){
+  if(err){
+    console.log(err);
+  } else {
+    for(var i = 0; i<rows.length; i++){
+      console.log(rows[i].author);
+    }
+  }
+})
+*/
+
+/*
 var sql = 'INSERT INTO topic (title, description, author) VALUES(?,?,?)';
 var params = ['Super', 'watcherDesc', 'graphauthor'];
 conn.query(sql.params,function(err,rows,fields){
@@ -27,4 +31,29 @@ conn.query(sql.params,function(err,rows,fields){
     console.log(rows.insertId);
   }
 });
+*/
+
+/* Update
+const sql = 'UPDATE topic SET title=?, author=? WHERE id=?';
+var params = ['NPM', 'JBE', 1];
+conn.query(sql,params,function(err,rows,fields){
+  if(err){
+    console.log(err);
+  } else {
+    console.log(rows);
+  }
+});
+ */
+
+/* Delete */
+const sql = 'DELETE FROM topic WHERE id=?';
+var params = [1];
+conn.query(sql,params,function(err,rows,fields){
+  if(err){
+    console.log(err);
+  } else {
+    console.log(rows);
+  }
+});
+
 conn.end();
